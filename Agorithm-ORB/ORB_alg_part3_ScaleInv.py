f2ee863dc88018c71951808153c7b9bed9eb6ded
Rafael1s <klivlend1@yahoo.com>
2019-01-11 15:48:30 +0200

    Added Scale-Invariant case for ORB algorithm

---
+++ Agorithm-ORB/ORB_alg_part3_ScaleInv.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 29 21:06:01 2018
+
+@author: Rafael Stekolshchik
+"""
+
+'''ORB's Main Properties
+
+
+Scale Invariance
+
+The ORB algorithm is scale invariant. This means that it is able to detect objects
+ in images regardless of their size. To see this, we will now 
+ use our Brute-Force matcher to match points between the training image 
+ and a query image that is a ¼ the size of the original training image.'''
+ 
+import cv2
+import matplotlib.pyplot as plt
+
+# Set the default figure size
+plt.rcParams['figure.figsize'] = [14.0, 7.0]
+
+# Load the training image
+image1 = cv2.imread('./images/face.jpeg')
+
+# Load the query image
+image2 = cv2.imread('./images/faceQS.png')
+
+print('image2: ', len(image2))
+
+# Convert the training image to RGB
+training_image = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+
+# Convert the query image to RGB
+query_image = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+
+# Display the images
+plt.subplot(121)
+plt.title('Training Image')
+plt.imshow(training_image)
+plt.subplot(122)
+plt.title('Query Image')
+plt.imshow(query_image)
+plt.show()
+
+# Convert the training image to gray scale
+training_gray = cv2.cvtColor(training_image, cv2.COLOR_BGR2GRAY)
+
+# Convert the query image to gray scale
+query_gray = cv2.cvtColor(query_image, cv2.COLOR_BGR2GRAY)
+
+# Set the parameters of the ORB algorithm by specifying the maximum number of keypoints to locate and
+# the pyramid decimation ratio
+orb = cv2.ORB_create(1000, 2.0)
+
+# Find the keypoints in the gray scale training and query images and compute their ORB descriptor.
+# The None parameter is needed to indicate that we are not using a mask in either case.
+keypoints_train, descriptors_train = orb.detectAndCompute(training_gray, None)
+keypoints_query, descriptors_query = orb.detectAndCompute(query_gray, None)
+
+# Create a Brute Force Matcher object. Set crossCheck to True so that the BFMatcher will only return consistent
+# pairs. Such technique usually produces best results with minimal number of outliers when there are enough matches.
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+
+# Perform the matching between the ORB descriptors of the training image and the query image
+matches = bf.match(descriptors_train, descriptors_query)
+
+# The matches with shorter distance are the ones we want. So, we sort the matches according to distance
+matches = sorted(matches, key = lambda x : x.distance)
+
+'''# Connect the keypoints in the training image with their best matching keypoints in the query image.
+# The best matches correspond to the first elements in the sorted matches list, since they are the ones
+# with the shorter distance. We draw the first 30 mathces and use flags = 2 to plot the matching keypoints
+# without size or orientation.'''
+result = cv2.drawMatches(training_gray, keypoints_train, query_gray, keypoints_query, matches[:30], query_gray, flags = 2)
+
+# Display the best matching points
+plt.title('Best Matching Points')
+plt.imshow(result)
+plt.show()
+
+# Print the shape of the training image
+print('\nThe Training Image has shape:', training_gray.shape)
+
+#Print the shape of the query image
+print('The Query Image has shape:', query_gray.shape)
+
+# Print the number of keypoints detected in the training image
+print("\nNumber of Keypoints Detected In The Training Image: ", len(keypoints_train))
+
+# Print the number of keypoints detected in the query image
+print("Number of Keypoints Detected In The Query Image: ", len(keypoints_query))
+
+# Print total number of matching points between the training and query images
+print("\nNumber of Matching Keypoints Between The Training and Query Images: ", len(matches))
+
+'''In the above example, notice that the training image is 553 x 471 pixels, 
+while the query image is 138 x 117 pixels, ¼ the size of the original training image.
+ Also notice that the number of keypoints detected in the query image is only 65, 
+ much smaller than the 831 keypoints found in the training image. 
+ Nevertheless, we can see that our Brute-Force matcher 
+ can match most of the keypoints in the query image 
+ with their corresponding keypoints in the training image.
+'''
+
+''' Result: The Training Image has shape: (553, 471)
+The Query Image has shape: (138, 117)
+
+Number of Keypoints Detected In The Training Image:  821
+Number of Keypoints Detected In The Query Image:  65
+
+Number of Matching Keypoints Between The Training and Query Images:  59
+'''
--- Agorithm-ORB/README.md
+++ Agorithm-ORB/README.md
@@ -1,4 +1,8 @@
 
+ 
+Again, in order to see the properties of the ORB algorithm more clearly, 
+in the following examples we will use the same image as our training and query image.
+
 ## Fast Local Feature Detector ORB
 
 Oriented FAST and rotated BRIEF (ORB) is a fast robust local feature detector, 
@@ -26,15 +30,28 @@ with their best matching keypoints in the query image,
 the result images are in the subdirectory 'match_keypoints'.
 
 ## ORB Invariance Properties
-1) ORB_alg_part4_RotateInv.py 
+We explore each of the main properties of the ORB algorithm:
+
+ * Scale Invariance
+ * Rotational Invariance
+ * Illumination Invariance
+ * Noise Invariance
+
+ ORB_alg_part3_ScaleInv
+   * get keypoints for the image face.jpeg and its rotated case faceQS.jpeg     
+   * Connect the keypoints in the training image with their best matching keypoints
+     in the query image. The best matches correspond to the first elements 
+     in the sorted matches list, since they are the ones with the shorter distance
+     
+ ORB_alg_part4_RotateInv.py 
    * get keypoints for the image face.jpeg and its rotated case faceR.jpeg       
    * compare obtained keypoints, see the result file rotate_match_keypoints.png
    
-2) ORB_alg_part6_NoiseInv.py
+ ORB_alg_part6_NoiseInv.py
    * get keypoints for the image face.jpeg and its rotated and noised case faceRN5.jpeg       
    * compare obtained keypoints, see the result file match_noise_keypoints.png
 
-3) ORB_alg_part7_ObjectDet.py 
+ ORB_alg_part7_ObjectDet.py 
    * find the face in the image Team.jpeg
    * gets keypoints for the image face.jpeg and its mapping in faceRN5.jpeg       
    * compare obtained keypoints, see the result file  team_match_leypoints.jpeg
